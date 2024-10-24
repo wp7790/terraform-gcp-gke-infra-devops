09bf2e25ec4352f9cae4fbb0820e7c18441eb7c8
Yonas Ayele <yonasayele@hotmail.com>
2024-10-24 02:41:16 -0400

    Base commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+
+
+# terraform-gcp-gke-devops1
+Implement DevOps Pipelines for Terraform Configs on GCP GKE (Google Kubernetes Engine) Resources
---
+++ get-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Add files and do local commit"
+git add .
+git commit -am "Welcome to wp2323"
+
+echo "Pushing to Github Repository"
+git push
